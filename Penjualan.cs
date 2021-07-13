66d6353edbd5763173a788d8240a7d290045242e
User <User@DESKTOP-0ISSLU1>
2021-07-12 23:14:46 -0700

    Add project files.

---
+++ Penjualan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal
+        public int Nota { get; set; }
+        public string tanggal { get; set; }
+        public string Nama { get; set; }
+        public char jenis { get; set; }
+        public double Total { get; set; }
+    }
+}
